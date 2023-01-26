99bd9e2c435ce7668efff6cf5c2634aef80fb5dc
Restyled.io <commits@restyled.io>
2023-01-26 15:23:37 +0000

    Restyled by standardrb

--- vidmerger.rb
+++ vidmerger.rb
@@ -4,7 +4,7 @@ class Vidmerger < Formula
   url "https://github.com/tgotwig/vidmerger/releases/download/0.3.1/vidmerger-linux.tar.gz"
   version "0.3.1"
   sha256 "4639451ab1d31fc509dc950df7f2d1a8f7e718ef6d2d0844f936deba1cf4b74b"
-  
+
   depends_on "ffmpeg"
 
   def install
