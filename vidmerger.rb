2d3c585ef8e6974ba3f3245d9a538718eb77139e
♠️ Thomas Gotwig ♣️ <ramble.clef0y@icloud.com>
2024-01-14 14:31:31 +0100

    Update version to 0.3.2

--- vidmerger.rb
+++ vidmerger.rb
@@ -1,8 +1,8 @@
 class Vidmerger < Formula
   desc "Wrapper around ffmpeg which simlifies merging multiple videos 🎞"
   homepage "https://github.com/TGotwig/vidmerger"
-  url "https://github.com/tgotwig/vidmerger/releases/download/0.3.1/vidmerger-linux.tar.gz"
-  version "0.3.1"
+  url "https://github.com/tgotwig/vidmerger/releases/download/0.3.2/vidmerger-linux.tar.gz"
+  version "0.3.2"
   sha256 "4639451ab1d31fc509dc950df7f2d1a8f7e718ef6d2d0844f936deba1cf4b74b"
 
   depends_on "ffmpeg"
